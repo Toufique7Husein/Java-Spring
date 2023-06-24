e2c3b9ef2e0e3efc643bffdfb2f5a915972baa70
tfq <tfq55390@gmail.com>
2023-06-24 16:51:18 +0600

    Merge branch 'main' of https://github.com/Toufique7Husein/Java-Spring.git into main

--- Spring-Practice/src/main/java/com/spring/practice/Spring_Practice/App.java
+++ Spring-Practice/src/main/java/com/spring/practice/Spring_Practice/App.java
@@ -1,13 +1,27 @@
 package com.spring.practice.Spring_Practice;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.spring.practice.Spring_Practice.student.Student;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("com/spring/practice/Spring_Practice/student/student_confug.xml");
+		
+		JdbcTemplate j = (JdbcTemplate)applicationContext.getBean("jdbc", JdbcTemplate.class);
+		
+		
+		
+		String q = "insert into student(id, name, address) values(?,?,?)";
+		
+		int res =   j.update(q, 1, "Md Toufique Husein", "Mohakhali, Dhala");
+		
+		System.out.println(res);
+	}
 }
