37a82ff3a5631290c330628b98d607ca83ffb536
tfq <tfq55390@gmail.com>
2023-06-25 16:35:40 +0600

    jdbc with annoation

--- Spring-Practice/src/main/java/com/spring/practice/Spring_Practice/App.java
+++ Spring-Practice/src/main/java/com/spring/practice/Spring_Practice/App.java
@@ -1,8 +1,10 @@
 package com.spring.practice.Spring_Practice;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.objenesis.instantiator.basic.NewInstanceInstantiator;
@@ -10,6 +12,7 @@ import org.springframework.objenesis.instantiator.basic.NewInstanceInstantiator;
 import com.spring.practice.Spring_Practice.student.Student;
 import com.spring.practice.Spring_Practrice.Dao.Dao;
 import com.spring.practice.Spring_Practrice.Dao.StudentDao;
+import com.spring.practice.Spring_Practrice.withouthxml.JdbcConfigure;
 
 /**
  * Hello world!
@@ -17,10 +20,13 @@ import com.spring.practice.Spring_Practrice.Dao.StudentDao;
  */
 public class App {
 	public static void main(String[] args) {
-		Student student = new StudentDao().getStudent(3);
-		System.out.println(student);
+		ApplicationContext context = new AnnotationConfigApplicationContext(JdbcConfigure.class);
+		StudentDao student = context.getBean("dao", StudentDao.class);
+		debug(student.getStudents());
 		
-		
-		System.out.println(new StudentDao().getStudents());
+	}
+	
+	static void debug(Object...obj) {
+		System.err.println(Arrays.deepToString(obj));
 	}
 }
